bfe7729069d9fc33a029abace1bca7a6422f9684
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2020-10-24 19:16:01 +0900

    Update bombtimer.c

--- bombtimer.c
+++ bombtimer.c
@@ -3,7 +3,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <string.h>
+#include <avr/interrupt.h>
 
+#define BAUDRATE(x) ((F_CPU / 16 / x) - 1)
 #define sbi(x, y) (x |= (1 << y))  // x의 y 비트를 설정(1)
 #define cbi(x, y) (x &= ~(1 << y)) // x의 y 비트를 클리어(0)
 
@@ -125,18 +127,44 @@ void LCD_wString(char *str)
 	LCD_wData(*str++);
 }
 
+void uart_init(unsigned int baud)
+{
+	UBRR0H = (unsigned char)(baud >> 8);
+	UBRR0L = (unsigned char)baud;
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1<<RXCIE0);
+} // 8bit, no parity, 1 stop bit, TX enable, RX ISR enable
+
+void uart_write(unsigned char data)
+{
+	while (!(UCSR0A & (1 << UDRE0)));
+	// wait for sending
+	UDR0 = data; // send
+}
+
+void uart_string(char* str)
+{
+	while (*str)
+	uart_write(*str++);
+}
+
+ISR(USART0_RX_vect)
+{
+	unsigned char buf = UDR0;
+	uart_write(buf - 'a' + 'A');
+}
+
 
 
 void buzzer()
 {
-	DDRG =0xff; //부저
-	LCD_Init();
-	LCD_Cursor(0,6);
+	DDRG =0xff; //부저사용한 포트
+	LCD_Init(); // LCD초기화
+	LCD_Cursor(0,6); //cursor
 
-	LCD_wString("Bomb!!");
+	LCD_wString("Bomb!!"); //Bomb!! LCD에 출력하기
 
 
-
+	//1초동안 부저 울리기
 	PORTG = 0xff;
 	_delay_ms(1000);
 	PORTG = 0x00;
@@ -144,24 +172,75 @@ void buzzer()
 	
 }
 
+
+void ready()
+{
+	
+	 
+	 DDRA = 0xFF;
+	 DDRC = 0xFF;
+	 DDRD = 0x00; // 버튼
+	 DDRB= 0xff;
+	 
+}
+
+
 int main(void)
 {
-    char buffhour[4];
-    char buffmin[4];
-    char buffsecond[4];
-	char password[4] = {"123"};
-	char passbuf[4] = {"000"};
-	int hour= 0,min=0,second=10;
-	
-	DDRA = 0xFF;
-	DDRC = 0xFF;
-	DDRD = 0xff; // 버튼
+   ready();
+   uart_init(BAUDRATE(9600));
+   sei();
+   char buffhour[4],buffmin[4],buffsecond[4]; // 시,분,초에 대한변수 설정
+   char password[4] = {"123"}; // 원하는 비밀번호 설정
+   char passbuf[4] = {0,}; // 버튼으로 비밀번호 넣을 변수
+   int hour= 0,min=0,second=10;
+      
+   unsigned int toggle=0;
 	
 	LCD_Init();
-
+	uart_string("start");
+	while(1)
+	{
 		while(second >= 0)
 		{
 			
+	
+			_delay_ms(900);
+			
+			if((PIND &0x01) == 0) //버튼을 눌렀을때
+			{
+				//sprintf(passbuf, "%d", num1);
+				strcat(passbuf,"1");
+				uart_string(passbuf);
+				toggle =0;
+				_delay_ms(100);
+			}
+			else if((PIND &0x02) == 0) //버튼을 눌렀을때
+			{
+				//sprintf(passbuf, "%d", num2);
+				strcat(passbuf,"2");
+				uart_string(passbuf);
+				toggle =0;
+				_delay_ms(100);
+			}
+			else if((PIND &0x04) == 0) //버튼을 눌렀을때
+			{
+				//sprintf(passbuf, "%d", num3);
+				strcat(passbuf,"3");
+				uart_string(passbuf);
+				toggle =0;
+				_delay_ms(100);
+			}
+			
+			if(strcmp(passbuf,password) == 0)
+			{
+				LCD_Init();
+				LCD_Cursor(0,4);
+				
+				LCD_wString("success!!");
+				_delay_ms(1000);
+				exit(1);
+			}
 			
 			LCD_Cursor(0,3);
 			LCD_wString("Bomb TIME");
@@ -176,35 +255,14 @@ int main(void)
 			sprintf(buffsecond, "%02d ", second);
 			LCD_wString(buffsecond); // WESNET 문자열 출력
 			LCD_wString(" : ");
-			_delay_ms(1000);
-			second--;
-			/*
-			if((PIND & 0x01) == 0)
-			{
-				strcat(passbuf,"1");
-			}
-			if((PIND & 0x02) == 0)
-			{
-				strcat(passbuf,"2");
-			}
-			if((PIND & 0x04) == 0)
-			{
-				strcat(passbuf,"3");
-			}
 			
-			/*if(strcmp(password,passbuf) == 0) 
-			{
-				LCD_Init();
-				LCD_Cursor(0,6);
-				LCD_wString("success");
-				_delay_ms(1000);
-				
-			}*/
+			second--;
 			
 		}
 		
 		buzzer(); // 부저함수
-		
+		exit(1);
+	}
 	
 }
 
