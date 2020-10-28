225f9ba2ba937d361917ae7e48525b5cd422aa85
my0614 <aqi222@naver.com>
2020-10-28 09:56:48 +0900

    add : buzzer 주파수사용하기

---
+++ buzzer.c
@@ -0,0 +1,21 @@
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRB = 0x20;
+	ICR1 = 783;
+	OCR1A = 784;
+	TCCR1A = 0xC2;
+	TCCR1B = 0x1D;
+	
+	while(1)
+	{
+		OCR1A = 391;
+		_delay_ms(3000);
+		OCR1A = 784;
+		_delay_ms(3000);
+		
+	}
+}
