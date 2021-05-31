f5b5daecb827f32f013ddd0ffdb016241d74d79b
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-05-31 12:40:14 +0900

    Update uart_lcd.c
    프로젝트 최종

--- uart_lcd.c
+++ uart_lcd.c
@@ -1,4 +1,3 @@
-
 #define F_CPU 16000000UL
 #define BAUDRATE(x) ((F_CPU / 16 / x) - 1)
 #include <stdio.h>
@@ -31,142 +30,169 @@ int i=0; // 인덱스 변수
 
 // 텍스트 LCD로 부터 상태(명령)를 읽는 함수
 unsigned char LCD_rCommand(void){
-	unsigned char temp=1;
-	
-	LCD_DATA_DIR = 0X00;
-	
-	cbi(LCD_CON, LCD_RS); // 0번 비트 클리어, RS = 0, 명령
-	sbi(LCD_CON, LCD_RW); // 1번 비트 설정, RW = 1, 읽기
-	sbi(LCD_CON, LCD_E);  // 2번 비트 설정, E = 1
-	delay_us(1);
-	
-	temp = LCD_DATA_IN;      // 명령 읽기
-	delay_us(1);
-	
-	cbi(LCD_CON, LCD_E);  // 명령 읽기 동작 끝
-	
-	LCD_DATA_DIR = 0XFF;
-	delay_us(1);
-	
-	return temp;
+    unsigned char temp=1;
+    
+    LCD_DATA_DIR = 0X00;
+    
+    cbi(LCD_CON, LCD_RS); // 0번 비트 클리어, RS = 0, 명령
+    sbi(LCD_CON, LCD_RW); // 1번 비트 설정, RW = 1, 읽기
+    sbi(LCD_CON, LCD_E);  // 2번 비트 설정, E = 1
+    delay_us(1);
+    
+    temp = LCD_DATA_IN;      // 명령 읽기
+    delay_us(1);
+    
+    cbi(LCD_CON, LCD_E);  // 명령 읽기 동작 끝
+    
+    LCD_DATA_DIR = 0XFF;
+    delay_us(1);
+    
+    return temp;
 }
 
 // 텍스트 LCD의 비지 플래그 상태를 확인하는 함수
 char LCD_BusyCheck(unsigned char temp)
 {
-	return temp & 0x80;
+    return temp & 0x80;
 }
 
 // 텍스트 LCD에 명령을 출력하는 함수 - 단, 비지플래그 체크하지 않음
 void LCD_wCommand(char cmd)
 {
-	cbi(LCD_CON, LCD_RS); // 0번 비트 클리어, RS = 0, 명령
-	cbi(LCD_CON, LCD_RW); // 1번 비트 클리어, RW = 0, 쓰기
-	sbi(LCD_CON, LCD_E);  // 2번 비트 설정, E = 1
-	LCD_DATA = cmd;          // 명령 출력
-	delay_us(1);
-	cbi(LCD_CON, LCD_E);  // 명령 쓰기 동작 끝
-	delay_us(1);
+    cbi(LCD_CON, LCD_RS); // 0번 비트 클리어, RS = 0, 명령
+    cbi(LCD_CON, LCD_RW); // 1번 비트 클리어, RW = 0, 쓰기
+    sbi(LCD_CON, LCD_E);  // 2번 비트 설정, E = 1
+    LCD_DATA = cmd;          // 명령 출력
+    delay_us(1);
+    cbi(LCD_CON, LCD_E);  // 명령 쓰기 동작 끝
+    delay_us(1);
 }
 
 // 텍스트 LCD에 명령을 출력하는 함수 - 단, 비지플래그 체크함
 void LCD_wBCommand(char cmd)
 {
-	while(LCD_BusyCheck(LCD_rCommand()))
-	delay_us(1);
-	cbi(LCD_CON, LCD_RS); // 0번 비트 클리어, RS = 0, 명령
-	cbi(LCD_CON, LCD_RW); // 1번 비트 클리어, RW = 0, 쓰기
-	sbi(LCD_CON, LCD_E);  // 2번 비트 설정, E = 1
-	
-	LCD_DATA = cmd;          // 명령 출력
-	delay_us(1);
-	cbi(LCD_CON, LCD_E);  // 명령 쓰기 동작 끝
-	delay_us(1);
+    while(LCD_BusyCheck(LCD_rCommand()))
+    delay_us(1);
+    cbi(LCD_CON, LCD_RS); // 0번 비트 클리어, RS = 0, 명령
+    cbi(LCD_CON, LCD_RW); // 1번 비트 클리어, RW = 0, 쓰기
+    sbi(LCD_CON, LCD_E);  // 2번 비트 설정, E = 1
+    
+    LCD_DATA = cmd;          // 명령 출력
+    delay_us(1);
+    cbi(LCD_CON, LCD_E);  // 명령 쓰기 동작 끝
+    delay_us(1);
 }
 
 void LCD_Cursor(char col, char row)
 {
-	LCD_wBCommand(0x80 | (row + col * 0x40));
+    LCD_wBCommand(0x80 | (row + col * 0x40));
 }
 
 // 텍스트 LCD를 초기화하는 함수
 void LCD_Init(void)
 {
-	delay_ms(100);
-	// 비지 플래그를 체크하지 않는 Function Set
-	LCD_wCommand(0x38);
-	delay_ms(10);
-	// 비지 플래그를 체크하지 않는 Function Set
-	LCD_wCommand(0x38);
-	delay_us(200);
-	// 비지 플래그를 체크하지 않는 Function Set
-	LCD_wCommand(0x38);
-	delay_us(200);
-	
-	// 비지 플래그를 체크하는 Function Set
-	LCD_wBCommand(0x38);
-	// 비지 플래그를 체크하는 Display On/Off Control
-	LCD_wBCommand(0x0c);
-	// 비지 플래그를 체크하는 Clear Display
-	LCD_wBCommand(0x01);
+    delay_ms(100);
+    // 비지 플래그를 체크하지 않는 Function Set
+    LCD_wCommand(0x38);
+    delay_ms(10);
+    // 비지 플래그를 체크하지 않는 Function Set
+    LCD_wCommand(0x38);
+    delay_us(200);
+    // 비지 플래그를 체크하지 않는 Function Set
+    LCD_wCommand(0x38);
+    delay_us(200);
+    
+    // 비지 플래그를 체크하는 Function Set
+    LCD_wBCommand(0x38);
+    // 비지 플래그를 체크하는 Display On/Off Control
+    LCD_wBCommand(0x0c);
+    // 비지 플래그를 체크하는 Clear Display
+    LCD_wBCommand(0x01);
 }
 
 void LCD_wData(char dat)
 {
-	while(LCD_BusyCheck(LCD_rCommand()))
-	{
-		delay_us(1);
-	}
-	sbi(LCD_CON, LCD_RS);
-	cbi(LCD_CON, LCD_RW);
-	sbi(LCD_CON, LCD_E);
-	LCD_DATA = dat;
-	delay_us(1);
-	cbi(LCD_CON, LCD_E);
-	delay_us(1);
+    while(LCD_BusyCheck(LCD_rCommand()))
+    {
+        delay_us(1);
+    }
+    sbi(LCD_CON, LCD_RS);
+    cbi(LCD_CON, LCD_RW);
+    sbi(LCD_CON, LCD_E);
+    LCD_DATA = dat;
+    delay_us(1);
+    cbi(LCD_CON, LCD_E);
+    delay_us(1);
 }
 
 // 문자열 LCD에 출력하기
 void LCD_wString(char *str)
 {
-	while(*str)
-	LCD_wData(*str++); // 한개의 문자열 출력을 계속 반복
+    while(*str)
+    LCD_wData(*str++); // 한개의 문자열 출력을 계속 반복
 }
 
 // uart 통신을 위한 신호 맞추기
 void uart_init(unsigned int baud)
 {
-	UBRR0H = (unsigned char)(baud >> 8);
-	UBRR0L = (unsigned char)baud;
-	UCSR0B = (1 << TXEN0) | (1 << RXEN0) |			(1<<RXCIE0);
+    UBRR0H = (unsigned char)(baud >> 8);
+    UBRR0L = (unsigned char)baud;
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0) |            (1<<RXCIE0);
 }
 
 // uart 문자받기
 void uart_write(unsigned char data)
 {
-	while (!(UCSR0A & (1 << UDRE0)));
-	UDR0 = data;
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = data;
 }
 
 // uart 문자열 통신
 void uart_string(char* str)
 {
-	while (*str)
-	uart_write(*str++);
+    while (*str)
+    uart_write(*str++);
 }
 
-int main(void)
+// uart 통신하기
+interrupt[USART_RXC] void uart_recv(void)
+{
+    unsigned char buf = UDR0; // 버퍼 선언
+    switch(buf)
+    {
+        // uart 처음시작 0x02, 마지막 0x03이다.
+        case 0x02 : state = 1; return; // 첫번째 단어 시작
+        case '\0' : state = 2; i =0; break; // 공백을 기준으로 1,2번째 단어 나누기
+        case 0x03 : state = 0; i = 0;break; // 2번째 단어 끝
+    }
+    // state가 1일때는 첫번째 단어 arr에 계속 저장하기
+    if(state == 1)
+    {
+        arr[i] = buf;
+        i++;        
+    }
+    // state가 2일때는 두번째 단어 arr2에 계속 저장하기
+    if(state == 2)
+    {
+        arr2[i] = buf;
+        i++;        
+    }
+}
+
+    
+
+void main()
 {
 
     DDRA = 0xFF; // LCD
-	DDRC = 0xFF; // LCD 데이터핀
+    DDRC = 0xFF; // LCD 데이터핀
     DDRD = 0x00; // 버튼
-	uart_init(BAUDRATE(9600)); // uart통신 속도 맞추기
-	sei(); // ISR 실행
+    uart_init(BAUDRATE(9600)); // uart통신 속도 맞추기
+   // sei(); // ISR 실행
     LCD_Init(); // LCD 초기화
 
- 	while(1)
-	{
+     while(1)
+    {
         LCD_Init(); // LCD 초기화
         LCD_Cursor(0,2); // LCD 커서 위치 
         LCD_wString(arr); // 첫번째 단어 출력
@@ -174,41 +200,17 @@ int main(void)
 
         LCD_Cursor(1,2); // LCD 커서 위치
         LCD_wString(arr2); // 두번째 단어 출력
-		delay_ms(500); // 0.5초 딜레이
+        delay_ms(500); // 0.5초 딜레이
 
         if((PIND & 0x01) == 0) // 버튼을 누르면
         {
             LCD_Init(); // LCD 초기화
             delay_ms(1000); // 1초 딜레이
-            exit(1);
+            
+          
         }
-				
-			
-	}
+                
+            
+    }
 }
 
-// uart 통신하기
-ISR(USART0_RX_vect)
-{
-	unsigned char buf = UDR0; // 버퍼 선언
-	switch(buf)
-	{
-        // uart 처음시작 0x02, 마지막 0x03이다.
-		case 0x02 : state = 1; return; // 첫번째 단어 시작
-        case '\0' : state = 2; i =0; break; // 공백을 기준으로 1,2번째 단어 나누기
-		case 0x03 : state = 0; i = 0;break; // 2번째 단어 끝
-	}
-    // state가 1일때는 첫번째 단어 arr에 계속 저장하기
-	if(state == 1)
-	{
-		arr[i] = buf;
-		i++;		
-	}
-    // state가 2일때는 두번째 단어 arr2에 계속 저장하기
-    if(state == 2)
-	{
-		arr2[i] = buf;
-		i++;		
-	}
-
-}
