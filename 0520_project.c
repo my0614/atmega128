faf599d77ffe0dd9f46b1f392bad3036ef855110
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-06-01 15:09:47 +0900

    Update 0520_project.c
    외부인터럽트 버튼 및 LCD

--- 0520_project.c
+++ 0520_project.c
@@ -1,13 +1,17 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+int toggle = 0;
+int dis, i;
+
 #define TRIG 0
 #define ECHO 1
 #define SOUND_VELOCITY 340UL
 
 unsigned char arr[4]={0x10,0x20,0x40,0x80}; // led
 void Command_write(uint8_t command)
-
 {
 
 	PORTG &= ~0x02;	// RS LOW
@@ -94,73 +98,87 @@ void buzzer()
 }
 
 
-// 시작버튼 외부인터럽트
-ISR(INT4_vect)
-{
-	int toggle = 1;
-    buzzer();
-    _delay_ms(100);
-}
-// 스탑버튼 외부인터럽트
-ISR(INT5_vect)
-{
-	int toggle = 2;
-    buzzer();
-    _delay_ms(100);
-}
-
 int main(void)
 {
-	unsigned int dis,i;
-	int toggle = 0;
-    PORTE = 0x00; // 버튼
-    EICRB = 0x0a; // 4,5번 falling edge
-    EIMSK = 0x30; // INT4-7까지 허용
-	//DDRD = 0xff;
-	//DDRD = ((DDRE | (1<<TRIG)) & ~(1<<ECHO)); // 초음파센서
-
+	DDRC = 0xFF;	// data Output
+	DDRG = 0x07;	// control signal Output
+	PORTG = 0x00;	// RW -> LOW, RS -> LOW, E -> LOW
+	DDRE = 0x00;
+	EICRB = 0xff;
+	EICRA = 0x00;
+	DDRD = 0xff;
+	PORTC = 0x00;
+	EIMSK = 0xf0; // INT4-7까지 허용
+	EIFR = 0xf0;
+	sei();
+	_delay_ms(1000);
+	LCD_init();
 	while(1)
 	{
-        if(toggle == 1)
-        {
-            start(); // lcd 출력
-            DDRD = 0xff;
-            DDRD = ((DDRE | (1<<TRIG)) & ~(1<<ECHO)); // 초음파센서
-            
-            TCCR1B = 0x03;
-            PORTD &= ~(1<<TRIG);
-            _delay_us(10);
-            PORTD |= (1<<TRIG);
-            _delay_us(10);
-            PORTD &= ~(1<<TRIG);
-            while(!(PIND & (1<<ECHO)));
-            TCNT1 = 0x0000;
-            while(PIND & (1<<ECHO));
-            TCCR1B = 0x00;
-            dis = (unsigned int)(SOUND_VELOCITY * (TCNT1 * 4 / 2) / 1000);
-            
-            // 일정한 거리 감지되면 모터 정지
-            if(dis <= 80)
-            {
-                toggle = 2;
-            }
-            
-                // led 반복하기
-            for(i=0;i<4;i++)
-            {
-                PORTD = arr[i];
-                _delay_ms(500);
-                    
-		    }	
-		}
-		if(toggle == 2)
+		PORTD = 0xff;	
+		
+		if(toggle== 4)
 		{
-            end(); // lcd 출력
-			PORTD = 0x00; // led,초음파센서 off
-            PORTG = 0x00; // 부저 off
+			toggle = 4;
+			PORTB = 0x08;
+			_delay_ms(100);
+			for(int j=0;j<4;j++)
+			{
+				PORTD = arr[j];
+				_delay_ms(150);
+				PORTD = 0x00;
+				_delay_ms(150);
+				
+			}
 
+			DDRD = ((DDRE | (1<<TRIG)) & ~(1<<ECHO)); // 초음파센서
+			// led 반복하기
+			
+			TCCR1B = 0x03;
+			PORTD &= ~(1<<TRIG);
+			_delay_us(10);
+			PORTD |= (1<<TRIG);
+			_delay_us(10);
+			PORTD &= ~(1<<TRIG);
+			while(!(PIND & (1<<ECHO)));
+			TCNT1 = 0x0000;
+			while(PIND & (1<<ECHO));
+			TCCR1B = 0x00;
+			dis = (unsigned int)(SOUND_VELOCITY * (TCNT1 * 4 / 2) / 1000);
+			// 일정한 거리 감지되면 모터 정지
+			if(dis <= 80)
+			{
+				toggle = 5;
+			}	
+			
 		}
-		
-		
+		if(toggle == 5)
+		{
+			toggle = 5;
+			PORTB = 0x00;
+			PORTD = 0xff; // led,초음파센서 off
+			PORTG = 0x00; // 부저 off
+			_delay_us(500);
+			
+		}
 	}
+}
+
+SIGNAL(INT4_vect)
+{
+	cli();
+	toggle = 4;
+	start();
+	buzzer();
+	sei();
+}
+
+
+SIGNAL(INT5_vect)
+{
+	cli();
+	toggle = 5;
+	end();
+	buzzer();
+	sei();
 }
