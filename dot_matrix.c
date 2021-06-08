a4a72ea7abbb5b6782def8f4f1913740a8b2f3d9
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-06-09 08:12:04 +0900

    Update dot_matrix.c

--- dot_matrix.c
+++ dot_matrix.c
@@ -1,105 +1,104 @@
-// ATmega128의 레지스터 등이 정의되어 있음
+#define F_CPU 16000000UL
+
 #include<avr/io.h>
- 
-// _delay_ms() 함수 등이 정의되어 있음
 #include<util/delay.h>
- 
-char Y[8] = {
-        0B10000010,
-        0B01000100,
-        0B00101000,
-        0B00010000,
-        0B00010000,
-        0B00010000,
-        0B00010000,
-        0B00010000
-    };
+#include<stdio.h>
+#include <avr/interrupt.h>
 
-char G[8] = {
-        0B00000000,
-        0B00000000,
-        0B11111110,
-        0B10010000,
-        0B10010000,
-        0B10011110,
-        0B11010010,
-        0B01111110
-    };
+int toggle = 0;
+   
+unsigned char Y_color[8] = {
+   0B10000010,
+   0B01000100,
+   0B00101000,
+   0B00010000,
+   0B00010000,
+   0B00010000,
+   0B00010000,
+   0B00010000
+};
 
-char N[8] = {
-        0B00000000,
-        0B00000000,
-        0B00000000,
-        0B00000000,
-        0B00000000,
-        0B00000000,
-        0B00000000,
-        0B00000000
-    };
+unsigned char G_color[8] = {
+   0B00000000,
+   0B00000000,
+   0B11111110,
+   0B10010000,
+   0B10010000,
+   0B10011110,
+   0B11010010,
+   0B01111110
+};
 
-char G[8] = {
-        0B11111111,
-        0B10000001,
-        0B11111111,
-        0B10100000,
-        0B10010000,
-        0B10001000,
-        0B10000100,
-        0B10000010
-    };
+unsigned char N_[8] = {
+   0B00000000,
+   0B00000000,
+   0B00000000,
+   0B00000000,
+   0B00000000,
+   0B00000000,
+   0B00000000,
+   0B00000000
+};
 
-int show(char name)
+unsigned char R_color[8] = {
+   0B11111111,
+   0B10000001,
+   0B11111111,
+   0B10100000,
+   0B10010000,
+   0B10001000,
+   0B10000100,
+   0B10000010
+};
+
+void show(char *name)
 {
-	DDRA = 0B11111111;
-    DDRC = 0B11111111;
-	unsigned char i;        // 8비트의 변수 선언
-	 for(i = 0; i < 8; i++){
-            // 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
-            PORTC = (1<<(i));
-            // COLS 신호에 데이터 출력
-            PORTA = name[i]; 
-            _delay_ms(2);    // 2ms 동안 대기
-        }
+   DDRA = 0B11111111;
+   DDRC = 0B11111111;
+   unsigned char i; 
+   
+   for(i = 0; i < 8; i++){
+      // 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
+      PORTC = (1<<(i));
+      // COLS 신호에 데이터 출력
+      PORTA = name[i];
+      _delay_ms(2);    // 2ms 동안 대기
+   }
 }
 
 // C 언어의 주 실행 함수
 int main(void){
-	int toggle = 0;
-	DDRD = 0xff; // led 출력 (G :0x01, Y:0x02, R:0x04)
-	DDRF = 0x00; // 버튼 입력
 
-    while(1){
-       PORTD = 0x01; //G led
-	   _delay_ms(10000);
-	   PORTD = 0x02; //Y led
-	   _delay_ms(2000);
-	   PORTD = 0x04; //R led
-	   _delay_ms(10000);
-
-		if((PINF & 0x01) == 0) // 버튼을 클릭했으면
-		{
-			toggle = 1;
-		}
-		else
-		{
-			toggle = 2;
-		}
-		if(toggle == 1)
-		{
-			PORTD = 0x02; // Y led
-			_delay_ms(2000);
-			PORTD = 0x04; // R led
-			show('G');
-			_delay_ms(10000);
-			show('Y');
-			_delay_ms(2000);
-			toggle =2;
-		}
-		if(toggle ==2)
-		{
-			show(N);
-		}
-    }
- 
-    return 0;
+   DDRD = 0xff; // led 출력 (G :0x01, Y:0x02, R:0x04)
+   DDRE = 0x00; // 버튼 입력
+   
+   _delay_ms(100);
+   while(1){
+      if((PINE & 0x01) == 0) toggle = 1;
+      PORTD = 0x10; //G led
+      _delay_ms(10000);
+      if((PINE & 0x01) == 0) toggle = 1;
+      PORTD = 0x20; //Y led
+      _delay_ms(2000);
+      if((PINE & 0x01) == 0) toggle = 1;
+      PORTD = 0x40; //R led
+      _delay_ms(10000);
+      show('N_');
+      if((PINE & 0x01) == 0) toggle = 1;
+      if(toggle == 1)
+      {
+         PORTD = 0x20; // Y led
+         _delay_ms(2000);
+         PORTD = 0x40; // R led
+         show("G_color");
+         _delay_ms(10000);
+         show("Y_color");
+         _delay_ms(2000);
+         toggle = 0;
+      }
+      
+      
+   }
+   
+   return 0;
 }
