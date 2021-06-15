3362fb27ccc3af5905d2f3384420844123e69e0d
Kim Min-Young <48994053+my0614@users.noreply.github.com>
2021-06-15 10:16:55 +0900

    Update 0614_project.c
    도트매트릭스 수정

--- 0614_project.c
+++ 0614_project.c
@@ -19,31 +19,31 @@ unsigned char DOT_R[8] = {
 
 ISR(INT0_vect)
 {
-    
    toggle = 1;
 
    EIFR = (1<<INTF0);
 }
 
-void show(char *name)
+
+void Matrix(char *name)
 {
-   DDRA = 0xFF;
-   DDRC = 0xFF;
-   int i;
-   
-   for(i = 0; i < 8; i++){
-      // 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
-      PORTE = 0x01<<i;
-      // COLS 신호에 데이터 출력
-      PORTA = name[i];
-      _delay_ms(2);    // 2ms 동안 대기
-   }
+    DDRA = 0xff; //  도트매트릭스 행
+    DDRB = 0xff; // 도트매트릭스 열
+    for(i = 0; i < 8; i++)
+    {
+        // 8비트의 ROWS 신호 중 1비트만 논리 1로 출력
+        PORTB = (1<<(i));
+        // COLS 신호에 데이터 출력
+        PORTA = name[i]; 
+        _delay_ms(2);    // 2ms 동안 대기
+    }
+
 }
 
 // C 언어의 주 실행 함수
 int main(void){
 
-   DDRF = 0xFF; // led 출력 (G :0x01, Y:0x02, R:0x04)
+   DDRD = 0xFF; // led 출력 (G :0x01, Y:0x02, R:0x04)
    DDRE = 0x00; // 버튼 입력
    EICRB = 0xFF;
    EICRA = (2<<ISC00);
@@ -53,23 +53,22 @@ int main(void){
    _delay_ms(100);
    while(1){
       
-      PORTF = 0x01; //G led
-      state = PORTF;
+      PORTD = 0x01; //G led
       _delay_ms(10000);
-      PORTF = 0x02; //Y led
-      state = PORTF;
+      PORTD = 0x02; //Y led
       _delay_ms(2000);
-      PORTF = 0x04; //R led
-      state = PORTF;
+      PORTD = 0x04; //R led
       _delay_ms(10000);
-
       if(toggle == 1)
       {
           
         PORTF = 0x02; 
         _delay_ms(2000);
+        Matrix("DOT_G");
         PORTF = 0x04;
-        _delay_ms(12000);
+        _delay_ms(10000);
+        Matrix("DOT_Y");
+        _delay_ms(2000);
         toggle = 0;
 
       }
@@ -77,12 +76,4 @@ int main(void){
    }
    
    return 0;
-}
-
-
-void msec_delay(int n) /* 시간지연 함수 */
-{
-
-   for(; n>0;n--)
-   _delay_ms(1);
 }
